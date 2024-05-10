56feb28064a41abd95074e363fb92fee54e90153
raranto <rantorakotoarisoa0@gmail.com>
2024-05-10 10:04:21 +0300

    Sprint commit

---
+++ src/pack/FrontController.java
@@ -0,0 +1,29 @@
+package pack;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        String queryURL = request.getRequestURL().toString();
+        out.println("Request : " + queryURL.toString());
+    }
+}
