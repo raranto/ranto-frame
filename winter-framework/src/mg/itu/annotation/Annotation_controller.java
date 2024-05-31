d47dbeae9454f516ac675068fe0a13e26230eacd
raranto <rantorakotoarisoa0@gmail.com>
2024-05-31 18:52:50 +0300

    Add files via upload

---
+++ winter-framework/src/mg/itu/annotation/Annotation_controller.java
@@ -0,0 +1,16 @@
+package mg.itu.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation_controller
+ */
+@Retention (RetentionPolicy.RUNTIME)
+@Target (ElementType.TYPE)
+public @interface Annotation_controller {
+
+    
+}
